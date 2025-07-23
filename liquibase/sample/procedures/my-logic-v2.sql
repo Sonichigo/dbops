9a4d21efabf060111e2fe33902e03e23fc6b8a6e
Animesh Pathak <kurosakiichigo.songoku@gmail.com>
2025-07-23 14:49:39 +0530

    feat: new struct
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>

---
+++ liquibase/sample/procedures/my-logic-v2.sql
@@ -0,0 +1,5 @@
+CREATE OR ALTER PROCEDURE simple_test_procedure
+AS
+  BEGIN
+  PRINT 'Hello From The Database (version 2)!';
+END;
---
+++ liquibase/sample/procedures/my-logic-v3.sql
@@ -0,0 +1,6 @@
+CREATE OR ALTER PROCEDURE simple_test_procedure
+AS
+BEGIN
+  PRINT 'Hello From The Database (version 3)!';
+END;
+
